bd9f40759e28b9ba76e68d02a31c548a71efcdfa
SiddheshKanawade <siddhesh.kanawade@iitgn.ac.in>
2024-03-17 11:04:49 +0530

    device_to_cpu

--- models/vnn/modules.py
+++ models/vnn/modules.py
@@ -25,7 +25,8 @@ def get_graph_feature(x, k=20, idx=None, x_coord=None):
             idx = knn(x, k=k)
         else:  # fixed knn graph with input point coordinates
             idx = knn(x_coord, k=k)
-    device = torch.device("cuda")
+    #device = torch.device("cuda")
+    device = torch.device("cpu")
 
     idx_base = (
         torch.arange(0, batch_size, device=device).view(-1, 1, 1) * num_points
